4c04dd7e421fb2c0bb05dab6ebbcaa3d5c574494
Rafael Rodrigues <rafael7rodrigues@gmail.com>
2019-02-25 23:01:07 -0300

    Adicionando campo senha e melhorias no css

--- index.php
+++ index.php
@@ -5,13 +5,22 @@
 	</head>
 	<body>
     <?php
-        echo "Olá Mundo !!!";
+        //echo "Olá Mundo !!!";
     ?>
         <form action="index.php" method="post">
-            <label for="cLogin">Login:</label>
-            <input type="text" name="cLogin" id="cLogin">
+            <fieldset>
+                <label for="cLogin">Login:</label>
+                <input type="text" name="cLogin" id="cLogin">
+            </fieldset>
 
+            <fieldset>
+                <label for="cSenha">Senha:</label>
+                <input type="text" name="cSenha" id="cSenha">
+            </fieldset>
+
+            <fieldset>
             <input type="submit" name="Enviar" value="Enviar">
+            </fieldset>
         </form>
 	</body>
 </html>
