48cf8f37f80af21a169268a9f3ced9d98fb13f86
Rafael Rodrigues <rafael7rodrigues@gmail.com>
2019-02-26 00:20:09 -0300

    Marcação da home css já utilizando bootstrap

--- home.php
+++ home.php
@@ -1,5 +1,15 @@
-﻿<?php
-
-echo "Você está na pagina inicial";
-
-?>
+﻿<html>
+<head>
+    <title>Git</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/bootstrap.css" />
+</head>
+<body>
+    <div id="container" class="border border-danger w-75 mx-auto">
+        <div class="row h-75 m-0 p-2 text-center">
+            <div class="col-8 w-100 bg-secondary">teste</div>
+            <div class="col-4 bg-warning">teste</div>
+        </div>
+    </div>
+</body>
+</html>
